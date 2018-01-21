ea68eb9f93a41da1b8d29c80b85cf5794d1db7e7
andersonPapillon <andersonclaytonp@hotmail.com>
2018-01-21 18:22:55 -0200

    Meu primeiro commit

---
+++ src/main/webapp/js/javascript.js
@@ -0,0 +1,14 @@
+function validar(){
+			
+	usuario = document.getElementById("txtUsuario").value;
+	senha   = document.getElementById("txtSenha").value; 
+			
+	if(usuario === "" || senha ===""){
+		window.alert("Os campos usuario e senha estao vazios animal!");
+		return false
+	}else{
+		window.alert("Pode entrar no sistema");
+		return true;
+	}
+			
+}
